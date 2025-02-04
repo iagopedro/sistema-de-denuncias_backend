f150b84316c491c26ff02299ff210ca7756eebb9
Iago da Silva <iago.da.silva@avanade.com>
2025-02-03 22:54:46 -0300

    chore(): remove .idea from gitignore

--- src/main/java/org/pdsw/api_pdsw/controllers/LoginController.java
+++ src/main/java/org/pdsw/api_pdsw/controllers/LoginController.java
@@ -12,11 +12,9 @@ import org.springframework.web.bind.annotation.*;
 public class LoginController {
 
     private final UserService userService;
-    private final PasswordService passwordService;
 
-    public LoginController(UserService userService, PasswordService passwordService) {
+    public LoginController(UserService userService) {
         this.userService = userService;
-        this.passwordService = passwordService;
     }
 
     @PostMapping
